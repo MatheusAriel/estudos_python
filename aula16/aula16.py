f5b9a2cda6e7eb28facb5036a20c6266d68a758a
Matheus Ariel <matheusariel@gmail.com>
2022-03-14 20:09:35 -0300

    Segundo commit

--- aula16/aula16.py
+++ aula16/aula16.py
@@ -1,5 +1,16 @@
 lista = [1, 2, 3, 'A', 10.9, 'Carro']
 
-print(lista[-1])
+print(lista[-1])#exibe o ultimo elemento da lista
 for x in lista:
-    print(x)
+    #print('Lista:',x)
+    print(f'Lista:{x}')
+
+for n, letra in enumerate('Matheus Ariel'):
+    print(n, letra)
+
+print()
+
+
+count = 2
+for i in range(1,11):
+    print(f'{count}*{i} = {count*i}')
