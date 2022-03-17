b75da2e66085156d066e4a1849d35df712f4b4fa
Matheus Ariel <matheusariel@gmail.com>
2022-03-16 23:21:56 -0300

    novas aulas

--- Basico/aula17 (arrays)/aula17.py
+++ Basico/aula17 (arrays)/aula17.py
@@ -1,14 +1,11 @@
-
 #             0               1         2           3
 array = ['Playstation', 'Microsoft', 'Nintendo', 'Sega'];
 #            -4            -3          -2          -1
 
 
+array.append('Neo Geo')  # append insere no final da lista
 
-
-array.append('Neo Geo')#append insere no final da lista
-
-array.insert(0,'Capcom')#insere onde quer na lista
+array.insert(0, 'Capcom')  # insere onde quer na lista
 
 print(array)
 print(array[0])
@@ -17,41 +14,48 @@ print(array[:2])
 print(array[-3:])
 print('******************\n\n')
 
+l1 = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+l2 = [4, 5, 6]
 
-l1 = [1,2,3,5,6,7,8,9,10]
-l2 = [4,5,6]
-
-print(l1+l2)
+print(l1 + l2)
 l1.extend(l2)
 l1.append('BANANA')
 print(l1)
-l1.pop()#remove o ultimo elemento da lista
+l1.pop()  # remove o ultimo elemento da lista
 print(l1)
 
 l1.append('Maçã')
 print(l1)
-del(l1[-1])
+del (l1[-1])
 print(l1)
 
-del(l1[3:5])
+del (l1[3:5])
 print(l1)
 
 print(max(l1))
 print(min(l1))
 
-
-l3 = list(range(1,11))
+l3 = list(range(1, 11))
 print(l3)
 
-del(l3[2:5])
+del (l3[2:5])
 print(l3)
 
-
-l4= ['String', True, 10,-90.5]
+l4 = ['String', True, 10, -90.5]
 
 for x in l4:
     print(f'O tipo do elemento é {type(x)} e seu valor é: {x}')
 
+jogo1 = 'GTA'
+jogo2 = 'Bully'
+jogo3 = 'Max Payne'
 
+listJogos = [jogo1, jogo2, jogo3]
 
+print(listJogos)
 
+# o 3 recebera do terceiro pra frente
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+v1, v2, *v3 = lista
+
+print(v1, v2, v3)
