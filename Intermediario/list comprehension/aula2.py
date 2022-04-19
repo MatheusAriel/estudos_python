8aca761961460a540adc8a8bd3ee78ee76055213
Matheus Ariel <matheusariel@gmail.com>
2022-04-18 22:16:58 -0300

    Exercicio

--- Intermediario/list comprehension/aula2.py
+++ Intermediario/list comprehension/aula2.py
@@ -3,4 +3,5 @@ list_numeros = list(range(100))
 pares = [n for n in list_numeros if n % 2 == 0]
 impares = [n for n in list_numeros if n % 2 != 0]
 
-print(pares)
+print(f'Número pares: \n\t{pares}\n'
+      f'Números ímpares: \n\t{impares}')
