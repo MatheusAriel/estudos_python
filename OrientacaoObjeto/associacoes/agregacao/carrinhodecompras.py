545ec0c340556374af49a16f98c7e3d49fbe35ea
Matheus Ariel <matheusariel@gmail.com>
2022-04-27 10:43:15 -0300

    novas aulas

--- OrientacaoObjeto/associacoes/agregacao/carrinhodecompras.py
+++ OrientacaoObjeto/associacoes/agregacao/carrinhodecompras.py
@@ -7,17 +7,12 @@ class CarrinhoDeCompras:
         return self.__produtos
 
     def inserir_produto(self, produto):
-        self.__produtos.append(produto)
+        self.produtos.append(produto)
 
     def listar_produtos(self):
-        for i, p in enumerate(self.__produtos, 1):
-            print(f'Produto {i}: {p.nome} - R${p.preco}')
+        for i, p in enumerate(self.produtos, 1):
+            print(f'Produto {i}: {p.nome} - \tR${p.preco}')
 
     def somar_total(self):
-        # print(self.__produtos)
-        #total = [x for x['preco'] in self.__produtos]
-        #print(total)
-        total = 0
-        for x in self.__produtos:
-            total += x.preco
-        return f'Total do carrinho R${total}'
+        total = sum([x.preco for x in self.produtos])
+        return total
