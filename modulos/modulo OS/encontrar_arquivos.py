8b589ce078ad4262b316342a15b87dbd3f0c782c
Matheus Ariel <matheusariel@gmail.com>
2022-05-02 21:13:57 -0300

    Modifieds

--- modulos/modulo OS/encontrar_arquivos.py
+++ modulos/modulo OS/encontrar_arquivos.py
@@ -1,9 +1,39 @@
 import os
-import utils
 from datetime import datetime
 from locale import setlocale, LC_ALL
 
 setlocale(LC_ALL, 'pt_BR.utf-8')
+
+
+def formatar_tamanho(tamanho):
+    base = 1024
+    kilo = base
+    mega = base ** 2
+    giga = base ** 3
+    tera = base ** 4
+    peta = base ** 5
+
+    if tamanho < kilo:
+        texto = 'B'
+    elif tamanho < mega:
+        tamanho /= kilo
+        texto = 'K'
+    elif tamanho < giga:
+        tamanho /= mega
+        texto = 'M'
+    elif tamanho < tera:
+        tamanho /= giga
+        texto = 'G'
+    elif tamanho < peta:
+        tamanho /= tera
+        texto = 'T'
+    else:
+        tamanho /= peta
+        texto = 'P'
+
+    tamanho = round(tamanho, 2)
+    return f'{tamanho}{texto}'.replace('.', ',')
+
 
 while True:
     caminho_procura = input('Informe o caminho: ').lower()
@@ -42,7 +72,8 @@ while True:
                     print(f'Tamanho: {tamanho_arquivo}')
                     print(f'Tamanho Formatado: {utils.formatar_tamanho(tamanho_arquivo)}')
                     print('Data de criação do arquivo:', data_cricao.strftime('%A, %d de %B de %Y %H:%M:%S'))
-                    print('Data da última modificacação no arquivo:', data_modificacao.strftime('%A, %d de %B de %Y %H:%M:%S'))
+                    print('Data da última modificacação no arquivo:',
+                          data_modificacao.strftime('%A, %d de %B de %Y %H:%M:%S'))
                     print('-=-' * 100)
                 except PermissionError as error:
                     print(f'Sem permissão - Erro: {error}')
@@ -52,3 +83,28 @@ while True:
                     print(f'Erro desconhecido - Erro {error}')
 
     print(f'{i} arquivo(s) encontrados com o termo "{termo_procura}"\n')
+
+"""
+from glob import iglob
+
+# Nos retorna todos os arquivos do diretório raiz
+files = iglob('/users/Downloads/*', recursive=True)
+for file in iglob:
+    print(file)
+
+# Nos retorna todos os arquivos do diretório raiz e de seus sub-diretórios
+files = iglob('/root/Downloads/**', recursive=True)
+
+# Nos retorna os sub-diretórios do diretório raiz
+files = iglob('/root/Downloads/*/', recursive=True)
+
+# Nos retorna o diretório raiz e seus sub-diretórios
+files = iglob('/root/Downloads/**/', recursive=True)
+
+# Nos retorna todos os arquivos do diretório raiz que contenham a extensão "mp3"
+files = iglob('/root/Downloads/*.mp3', recursive=True)
+
+# Nos retorna todos os arquivos dos sub-diretórios que contenham a extensão "mp3"
+files = iglob('/root/Downloads/*/*.mp3', recursive=True)
+
+"""
