0c8c15ca1081e7e783e413236eba25d04833077c
Matheus Ariel <matheusariel@gmail.com>
2022-05-04 21:06:19 -0300

    Modifieds

--- modulos/web_scraping/main3.py
+++ modulos/web_scraping/main3.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from time import perf_counter
+import traceback
 
 ()
 
@@ -10,30 +11,55 @@ try:
     response = requests.get(url)
     html = BeautifulSoup(response.text, 'html.parser')
 
-    i = 1
     start = perf_counter()
     nro_noticias = 0
-    while i <= nro_paginas:
-        url = url.format(i)
+    i = 1
 
-        for noticia in html.select('.linkNoticia'):
-            titulo = noticia.select_one('.titulos.ultimas-noticias h3')
-            data = noticia.find_all('small', attrs={'class': '.listaData'}, class_=False, id=False)[0]
-            hora = noticia.find_all('small', attrs={'class': '.listaData'}, class_=False, id=False)[1]
-            tipo = noticia.find_all('small', attrs={'class': '.listaData'}, class_=False, id=False)[2]
-            descricao = noticia.select_one('.titulos.ultimas-noticias h5')
+    with open('noticias.txt', 'w') as file:
+        while i <= nro_paginas:
+            url = url.format(i)
 
-            nro_noticias += 1
-            print('*' * 100)
-            print(f'Data:\t{data.text} {hora.text}')
-            print(f'Título:\t{titulo.text}')
-            print(f'Tipo:\t{tipo.text}')
-            print(f'Descrição: {descricao.text.replace("&nbsp;;", "")}')
-            print('*' * 100, '\n\n')
-        i += 1
+            for noticia in html.select('.linkNoticia'):
+                titulo = noticia.select_one('.titulos.ultimas-noticias h3')
+                data = noticia.find_all('small', attrs={'class': '.listaData'}, class_=False, id=False)[0]
+                hora = noticia.find_all('small', attrs={'class': '.listaData'}, class_=False, id=False)[1]
+                tipo = noticia.find_all('small', attrs={'class': '.listaData'}, class_=False, id=False)[2]
+                descricao = noticia.select_one('.titulos.ultimas-noticias h5')
+                link_noticia = noticia.select_one('.titulos.ultimas-noticias')
 
-    end = perf_counter()
+                nro_noticias += 1
 
-    print(f'Total de páginas:{i-1} - noticias: {nro_noticias} - duração: {(end - start):.2f} segundos')
+                file.write('*' * 70)
+                file.write(f'\nData:\t{data.text} | {hora.text}')
+                file.write(f'\nTítulo:\t{titulo.text}')
+                file.write(f'\nTipo:\t{tipo.text}')
+                file.write(f'\nLink:\t{link_noticia["href"]}')
+                file.write(f'\nDescrição:\t{descricao.text.replace("&nbsp;", "")}\n')
+
+                """
+                response2 = requests.get(link_noticia['href'])
+                html2 = BeautifulSoup(response2.text, 'html.parser')
+                for conteudo in html2.select('.grid_10.alpha.prefix_1.suffix_1'):
+                    paragrafos = conteudo.find_all('p',class_=False, id=False)
+                    for paragrafo in paragrafos:
+                        print(paragrafo)
+                        if '<p ppad="true"' in paragrafo:
+                            print(paragrafo.text)
+                    # print(paragrafos[0])
+                    # print(paragrafos[0])
+
+                    # print(paragrafo.text)
+                """
+                file.write('*' * 70)
+                file.write('\n\n')
+
+            print(f'{i}/{nro_paginas}')
+            i += 1
+
+        end = perf_counter()
+        msg = f'\nTotal de páginas: {i - 1} - noticias: {nro_noticias} - duração: {(end - start):.2f} segundos'
+        file.write(msg)
+        print(msg)
 except:
+    print(traceback.print_exc())
     print('Erro')
