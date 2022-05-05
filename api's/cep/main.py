4190df8989edd25c097e5c0f64055a4080afc1ad
Matheus Ariel <matheusariel@gmail.com>
2022-05-04 22:33:34 -0300

    Modifieds

--- api's/cep/main.py
+++ api's/cep/main.py
@@ -1,6 +1,7 @@
 from cep import *
 from time import perf_counter
 from tqdm import tqdm
+import json
 
 while True:
     try:
@@ -12,12 +13,18 @@ while True:
 
 start_time = perf_counter()
 
-for i in tqdm(range(vezes)):
-    endereco = Cep.gerar_endereco()
-    cep = Cep(endereco['cep'], ApiCep.AWESOME)
-    cep.buscar_cep()
-    # print(dir(cep))#mostra todos os metodos e atributos da classe cep
-    # print('\t', f'Req: {i} - {cep.buscar_cep()}')
+with open('enderecos.txt', 'w') as file:
+    for i in tqdm(range(vezes)):
+        endereco = Cep.gerar_endereco()
+        cep = Cep(endereco['cep'], ApiCep.AWESOME)
+        cep.buscar_cep()
+
+        # mostra todos os metodos e atributos da classe cep
+        # print(dir(cep))
+
+        json.dump(endereco, file)
+        file.write('\n')
 
 end_time = perf_counter()
-print(f'Tempo total de processo {vezes} requisições: {(end_time - start_time):.2f} segundos usando a api {ApiCep.AWESOME.name}')
+print(
+    f'Tempo total de processo {vezes} requisições: {(end_time - start_time):.2f} segundos usando a api {ApiCep.AWESOME.name}')
