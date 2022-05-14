88c992fa85a3129d79ca54b8d9ab6e99a2218222
Matheus Ariel <matheusariel@gmail.com>
2022-05-14 00:50:15 -0300

    Modifieds

--- api's/cep/main.py
+++ api's/cep/main.py
@@ -6,23 +6,24 @@ import json
 while True:
     try:
         vezes = int(input('Informe quantas requisições você deseja ? '))
-        if vezes > 0:
-            with open('enderecos.json', 'a+') as file:
-                for i in tqdm(range(vezes)):
-                    try:
-                        endereco = Cep.gerar_endereco()
-                        cep = Cep(endereco['cep'], ApiCep.AWESOME)
-
-                        if not cep.buscar_cep() is None:
-                            # mostra todos os metodos e atributos da classe cep
-                            # print(dir(cep))
-
-                            json.dump(endereco, file, ensure_ascii=False)
-                            file.write(',\n')
-                    except:
-                        print('Erro')
-        elif vezes == -1:
+        if vezes <= 0:
             break
+
+        with open('enderecos.json', 'a+') as file:
+            for i in tqdm(range(vezes)):
+                try:
+                    endereco = Cep.gerar_endereco()
+                    cep = Cep(endereco['cep'], ApiCep.AWESOME)
+
+                    if not cep.buscar_cep() is None:
+                        # mostra todos os metodos e atributos da classe cep
+                        # print(dir(cep))
+
+                        json.dump(endereco, file, ensure_ascii=False)
+                        file.write(',\n')
+                except:
+                    print('Erro')
+
 
     except:
         print('informe um número válido')
